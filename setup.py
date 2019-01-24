5023bc0fbfe481861f729129bb06e23c1ac9329d
Netanel Revah <netanelrevah@users.noreply.github.com>
2019-01-25 00:00:46 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -16,5 +16,14 @@ setup(
     license="MIT License",
     python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     include_package_data=True,
-
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+  ],
 )
